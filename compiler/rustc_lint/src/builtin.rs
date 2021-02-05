05123a70f789349b6c3b1dbb9daf46d10190553a
Camille GILLOT <gillot.camille@gmail.com>
2021-02-05 15:47:44 +0100

    Merge the BTreeMap in hir::Crate.

--- compiler/rustc_lint/src/builtin.rs
+++ compiler/rustc_lint/src/builtin.rs
@@ -570,7 +570,7 @@ impl<'tcx> LateLintPass<'tcx> for MissingDoc {
     fn check_crate(&mut self, cx: &LateContext<'_>, krate: &hir::Crate<'_>) {
         self.check_missing_docs_attrs(cx, hir::CRATE_HIR_ID, krate.item.inner, "the", "crate");
 
-        for macro_def in krate.exported_macros {
+        for macro_def in krate.exported_macros() {
             let attrs = cx.tcx.hir().attrs(macro_def.hir_id());
             let has_doc = attrs.iter().any(|a| has_doc(cx.sess(), a));
             if !has_doc {
