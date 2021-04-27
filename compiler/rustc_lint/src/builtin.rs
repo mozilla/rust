2af5290b6aee41a20de6a375eb41d6ad7b980c50
Jack Huey <jack.huey@umassmed.edu>
2021-04-27 14:55:38 -0400

    Make GATs no longer incomplete

--- compiler/rustc_lint/src/builtin.rs
+++ compiler/rustc_lint/src/builtin.rs
@@ -2297,7 +2297,7 @@ declare_lint! {
     /// ### Example
     ///
     /// ```rust
-    /// #![feature(generic_associated_types)]
+    /// #![feature(const_generics)]
     /// ```
     ///
     /// {{produces}}
