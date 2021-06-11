0219825c66e2d54ddf5e26ddbdcb1fce98371ba8
Tomoaki Kawada <kawada@kmckk.co.jp>
2021-06-11 18:40:46 +0900

    kmc-solid: Don't set `TargetOptions::exe_suffix`
    The notion of "executable files" isn't well-defined for these targets
    yet. Therefore, it doesn't make sense to define `TargetOptions::exe_
    suffix` at this point.

--- compiler/rustc_target/src/spec/solid_base.rs
+++ compiler/rustc_target/src/spec/solid_base.rs
@@ -4,7 +4,6 @@ pub fn opts(kernel: &str) -> TargetOptions {
     TargetOptions {
         os: format!("solid-{}", kernel),
         vendor: "kmc".to_string(),
-        exe_suffix: ".out".to_string(),
         has_elf_tls: true,
         ..Default::default()
     }
