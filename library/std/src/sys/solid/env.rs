ea989135b28910537f3d5b2d848d0b56b7609da8
Tomoaki Kawada <kawada@kmckk.co.jp>
2021-06-11 18:48:56 +0900

    kmc-solid: Fix `DLL_EXTENSION`
    Co-authored-by: bjorn3 <bjorn3@users.noreply.github.com>

--- library/std/src/sys/solid/env.rs
+++ library/std/src/sys/solid/env.rs
@@ -3,7 +3,7 @@ pub mod os {
     pub const OS: &str = "solid";
     pub const DLL_PREFIX: &str = "";
     pub const DLL_SUFFIX: &str = ".so";
-    pub const DLL_EXTENSION: &str = ".so";
+    pub const DLL_EXTENSION: &str = "so";
     pub const EXE_SUFFIX: &str = "";
     pub const EXE_EXTENSION: &str = "";
 }
