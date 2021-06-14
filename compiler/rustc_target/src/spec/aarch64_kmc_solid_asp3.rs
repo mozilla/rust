7abde323b3dded108d06764f5651fd8e3a7af44b
Tomoaki Kawada <kawada@kmckk.co.jp>
2021-06-14 10:51:31 +0900

    kmc-solid: Use `*-gcc` for `TargetOptions::linker`

--- compiler/rustc_target/src/spec/aarch64_kmc_solid_asp3.rs
+++ compiler/rustc_target/src/spec/aarch64_kmc_solid_asp3.rs
@@ -8,7 +8,7 @@ pub fn target() -> Target {
         data_layout: "e-m:e-i8:8:32-i16:16:32-i64:64-i128:128-n32:64-S128".to_string(),
         arch: "aarch64".to_string(),
         options: TargetOptions {
-            linker: Some("aarch64-kmc-elf-g++".to_owned()),
+            linker: Some("aarch64-kmc-elf-gcc".to_owned()),
             features: "+strict-align,+neon,+fp-armv8".to_string(),
             relocation_model: RelocModel::Static,
             disable_redzone: true,
