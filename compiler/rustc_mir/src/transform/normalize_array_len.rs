3b86d25215be2cd3673015ebdd7db45218f5d0fc
Alex Vlasov <alex.m.vlasov@gmail.com>
2021-06-22 00:48:40 +0200

    test & bless, some MIR tests should be removed due to const prop

--- compiler/rustc_mir/src/transform/normalize_array_len.rs
+++ compiler/rustc_mir/src/transform/normalize_array_len.rs
@@ -33,7 +33,7 @@ pub fn normalize_array_len_calls<'tcx>(tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>)
             local_decls,
             &mut state,
             &mut patches_scratchpad,
-            &mut replacements_scratchpad
+            &mut replacements_scratchpad,
         );
         for el in state.iter_mut() {
             *el = None;
@@ -52,7 +52,10 @@ struct Patcher<'a, 'tcx> {
 }
 
 impl<'a, 'tcx> Patcher<'a, 'tcx> {
-    fn patch_expand_statement(&mut self, statement: &mut Statement<'tcx>) -> Option<std::vec::IntoIter<Statement<'tcx>>> {
+    fn patch_expand_statement(
+        &mut self,
+        statement: &mut Statement<'tcx>,
+    ) -> Option<std::vec::IntoIter<Statement<'tcx>>> {
         let idx = self.statement_idx;
         if let Some(len_statemnt_idx) = self.patches_scratchpad.get(&idx).copied() {
             let mut statements = Vec::with_capacity(2);
@@ -62,12 +65,20 @@ impl<'a, 'tcx> Patcher<'a, 'tcx> {
             // use this copy in the Len operation
 
             match &statement.kind {
-                StatementKind::Assign(box(.., Rvalue::Cast(CastKind::Pointer(ty::adjustment::PointerCast::Unsize), operand, _))) => {
+                StatementKind::Assign(box (
+                    ..,
+                    Rvalue::Cast(
+                        CastKind::Pointer(ty::adjustment::PointerCast::Unsize),
+                        operand,
+                        _,
+                    ),
+                )) => {
                     match operand {
                         Operand::Copy(place) | Operand::Move(place) => {
                             // create new local
                             let ty = operand.ty(self.local_decls, self.tcx);
-                            let local_decl = LocalDecl::with_source_info(ty, statement.source_info.clone());
+                            let local_decl =
+                                LocalDecl::with_source_info(ty, statement.source_info.clone());
                             let local = self.local_decls.push(local_decl);
                             // make it live
                             let mut make_live_statement = statement.clone();
@@ -79,7 +90,8 @@ impl<'a, 'tcx> Patcher<'a, 'tcx> {
                             let mut make_copy_statement = statement.clone();
                             let assign_to = Place::from(local);
                             let rvalue = Rvalue::Use(operand);
-                            make_copy_statement.kind = StatementKind::Assign(box(assign_to, rvalue));
+                            make_copy_statement.kind =
+                                StatementKind::Assign(box (assign_to, rvalue));
                             statements.push(make_copy_statement);
 
                             // to reorder we have to copy and make NOP
@@ -87,11 +99,15 @@ impl<'a, 'tcx> Patcher<'a, 'tcx> {
                             statement.make_nop();
 
                             self.replacements_scratchpad.insert(len_statemnt_idx, local);
-                        },
-                        _ => {unreachable!("it's a bug in the implementation")}
+                        }
+                        _ => {
+                            unreachable!("it's a bug in the implementation")
+                        }
                     }
-                },
-                _ => {unreachable!("it's a bug in the implementation")}
+                }
+                _ => {
+                    unreachable!("it's a bug in the implementation")
+                }
             }
 
             self.statement_idx += 1;
@@ -101,7 +117,7 @@ impl<'a, 'tcx> Patcher<'a, 'tcx> {
             let mut statements = Vec::with_capacity(2);
 
             match &statement.kind {
-                StatementKind::Assign(box(into, Rvalue::Len(place))) => {
+                StatementKind::Assign(box (into, Rvalue::Len(place))) => {
                     let add_deref = if let Some(..) = place.as_local() {
                         false
                     } else if let Some(..) = place.local_or_deref_local() {
@@ -115,7 +131,7 @@ impl<'a, 'tcx> Patcher<'a, 'tcx> {
                     if add_deref {
                         place = self.tcx.mk_place_deref(place);
                     }
-                    len_statement.kind = StatementKind::Assign(box(*into, Rvalue::Len(place)));
+                    len_statement.kind = StatementKind::Assign(box (*into, Rvalue::Len(place)));
                     statements.push(len_statement);
 
                     // make temporary dead
@@ -125,8 +141,10 @@ impl<'a, 'tcx> Patcher<'a, 'tcx> {
 
                     // make original statement NOP
                     statement.make_nop();
-                },
-                _ => {unreachable!("it's a bug in the implementation")}
+                }
+                _ => {
+                    unreachable!("it's a bug in the implementation")
+                }
             }
 
             self.statement_idx += 1;
@@ -145,21 +163,26 @@ fn normalize_array_len_call<'tcx>(
     local_decls: &mut IndexVec<Local, LocalDecl<'tcx>>,
     state: &mut IndexVec<Local, Option<usize>>,
     patches_scratchpad: &mut FxIndexMap<usize, usize>,
-    replacements_scratchpad: &mut FxIndexMap<usize, Local>
+    replacements_scratchpad: &mut FxIndexMap<usize, Local>,
 ) {
     for (statement_idx, statement) in block.statements.iter_mut().enumerate() {
         match &mut statement.kind {
             StatementKind::Assign(box (place, rvalue)) => {
                 match rvalue {
-                    Rvalue::Cast(CastKind::Pointer(ty::adjustment::PointerCast::Unsize), operand, cast_ty) => {
-                        let local = if let Some(local) = place.as_local() {local} else {return};
+                    Rvalue::Cast(
+                        CastKind::Pointer(ty::adjustment::PointerCast::Unsize),
+                        operand,
+                        cast_ty,
+                    ) => {
+                        let local = if let Some(local) = place.as_local() { local } else { return };
                         match operand {
                             Operand::Copy(place) | Operand::Move(place) => {
-                                let operand_local = if let Some(local) = place.local_or_deref_local() {
-                                    local
-                                } else {
-                                    return
-                                };
+                                let operand_local =
+                                    if let Some(local) = place.local_or_deref_local() {
+                                        local
+                                    } else {
+                                        return;
+                                    };
                                 let operand_ty = local_decls[operand_local].ty;
                                 match (operand_ty.kind(), cast_ty.kind()) {
                                     (ty::Array(of_ty_src, ..), ty::Slice(of_ty_dst)) => {
@@ -167,9 +190,16 @@ fn normalize_array_len_call<'tcx>(
                                             // this is a cast from [T; N] into [T], so we are good
                                             state[local] = Some(statement_idx);
                                         }
-                                    },
+                                    }
                                     // current way of patching doesn't allow to work with `mut`
-                                    (ty::Ref(ty::RegionKind::ReErased, operand_ty, Mutability::Not), ty::Ref(ty::RegionKind::ReErased, cast_ty, Mutability::Not)) => {
+                                    (
+                                        ty::Ref(
+                                            ty::RegionKind::ReErased,
+                                            operand_ty,
+                                            Mutability::Not,
+                                        ),
+                                        ty::Ref(ty::RegionKind::ReErased, cast_ty, Mutability::Not),
+                                    ) => {
                                         match (operand_ty.kind(), cast_ty.kind()) {
                                             // current way of patching doesn't allow to work with `mut`
                                             (ty::Array(of_ty_src, ..), ty::Slice(of_ty_dst)) => {
@@ -177,32 +207,32 @@ fn normalize_array_len_call<'tcx>(
                                                     // this is a cast from [T; N] into [T], so we are good
                                                     state[local] = Some(statement_idx);
                                                 }
-                                            },
+                                            }
                                             _ => {}
                                         }
-                                    },
-                                    _ => {},
+                                    }
+                                    _ => {}
                                 }
-                            },
+                            }
                             _ => {}
                         }
-                    },
+                    }
                     Rvalue::Len(place) => {
                         let local = if let Some(local) = place.local_or_deref_local() {
                             local
                         } else {
-                            return
+                            return;
                         };
                         if let Some(cast_statement_idx) = state[local] {
                             patches_scratchpad.insert(cast_statement_idx, statement_idx);
                         }
-                    },
+                    }
                     _ => {
                         // invalidate
                         state[place.local] = None;
-                    },
+                    }
                 }
-            },
+            }
             _ => {}
         }
     }
@@ -215,7 +245,5 @@ fn normalize_array_len_call<'tcx>(
         statement_idx: 0,
     };
 
-    block.expand_statements(|st| {
-        patcher.patch_expand_statement(st)
-    });
+    block.expand_statements(|st| patcher.patch_expand_statement(st));
 }
