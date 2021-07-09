8748d8a4f54af0322691b51ee90d48e152a6b064
Mara Bos <m-ou.se@m-ou.se>
2021-07-09 17:39:37 +0200

    Make const_fmt_args!() work during bootstrap.

--- library/core/src/macros/mod.rs
+++ library/core/src/macros/mod.rs
@@ -832,6 +832,7 @@ pub(crate) mod builtin {
     }
 
     /// Same as `format_args`, but can be used in some const contexts.
+    #[cfg(not(bootstrap))]
     #[unstable(feature = "const_format_args", issue = "none")]
     #[allow_internal_unstable(fmt_internals, const_fmt_arguments_new)]
     #[rustc_builtin_macro]
@@ -839,6 +840,16 @@ pub(crate) mod builtin {
     macro_rules! const_format_args {
         ($fmt:expr) => {{ /* compiler built-in */ }};
         ($fmt:expr, $($args:tt)*) => {{ /* compiler built-in */ }};
+    }
+
+    /// Same as `format_args`, but can be used in some const contexts.
+    #[cfg(bootstrap)]
+    #[unstable(feature = "const_format_args", issue = "none")]
+    #[macro_export]
+    macro_rules! const_format_args {
+        ($($t:tt)*) => {
+            $crate::format_args!($($t)*)
+        }
     }
 
     /// Same as `format_args`, but adds a newline in the end.
