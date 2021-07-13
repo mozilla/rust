6a6ee83fb0d67e00ce0cff67fe7d80678ef12b7a
Richard Cobbe <ricobbe@microsoft.com>
2021-07-13 15:14:49 -0700

    Adopt petrochenkov's solution; update test to match.  Test still failing; would love feedback on why.

--- src/test/ui/rfc-2627-raw-dylib/multiple-declarations.rs
+++ src/test/ui/rfc-2627-raw-dylib/multiple-declarations.rs
@@ -1,4 +1,5 @@
 // only-i686-pc-windows-msvc
+// build-pass
 // compile-flags: --crate-type lib --emit link
 #![allow(clashing_extern_declarations)]
 #![feature(raw_dylib)]
@@ -12,7 +13,7 @@ pub fn lib_main() {
     #[link(name = "foo", kind = "raw-dylib")]
     extern "stdcall" {
         fn f(x: i32);
-        //~^ ERROR multiple definitions of external function `f` from library `foo.dll` have different calling conventions
+        //~^ ERROR multiple declarations of external function `f` from library `foo.dll` have different calling conventions
     }
 
     unsafe { f(42); }
